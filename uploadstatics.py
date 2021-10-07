f81e67a0eab4a61c070f58211760400fb010c821
virus al7rbi <fahadalwasidi@gmail.com>
2021-10-07 09:59:05 +0300

    pycares fixed, edit upload statics

--- uploadstatics.py
+++ uploadstatics.py
@@ -6,7 +6,7 @@ from aiohttp import ClientSession
 from pprint import pprint
 
 BASE_DIR = os.getcwd()
-all_files = [os.path.join(root, file).split("GitHub")[1] for root, dirs, files in os.walk(os.path.abspath(os.getcwd() + "/static/")) for file in files]
+all_files = [os.path.join(root, file) for root, dirs, files in os.walk(os.path.abspath(os.getcwd() + "/static/")) for file in files]
 
 async def fetch(session, path):
     file_path = os.path.join(BASE_DIR,path.split("al7rbi_site/")[1])
@@ -24,4 +24,4 @@ async def fetch_all(urls, loop):
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     urls = all_files
-    htmls = loop.run_until_complete(fetch_all(urls, loop))
+    upload_static = loop.run_until_complete(fetch_all(urls, loop))
